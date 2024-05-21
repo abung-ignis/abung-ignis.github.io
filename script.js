192f0680f06fc44e3e6e10e80c6d91940ecc813b
abung-ignis <86881903+abung-ignis@users.noreply.github.com>
2024-05-21 23:44:29 +0800

    Still learning js
    ~3:22:39

--- script.js
+++ script.js
@@ -187,3 +187,137 @@
 // var helloFunctionExpression = function(){
 //     console.log('awa magic cenario')
 // }
+
+//function的scope
+//function裏的variable是只限於function內的 (local)
+//如果在function裏用到出面宣告了的variable但你沒給他套var之類的宣告字眼
+//那個本來在出面的variable也會被你function裏的code更改掉值
+// var a = 100;
+// function helloWorld(){
+//     var a = 150;
+// }
+// helloWorld()
+// console.log(a)
+
+//global variable & local variable
+//宣告variable有三種宣告方式
+//可以用var、let或const來宣告
+//在function裏以上三種來宣告變數變local (只在function裏變)
+//在code裏直接用那三種來宣變數變global (不用講都知道是什麼吧)
+//可以在chrome console裏用window.(變數名稱) 看到某variable的值
+//啊對，window是根物件，就是代表網頁的整個物件
+//在function裏三種宣告都不用的話，他就單純是全域的屬性
+// var awa = 10;
+// function ale(){
+//     var x = 1;
+//     z = 1;
+// }
+
+//callback function 回呼函數
+//函數也可以當另一個函數裏的參數來用!
+//目前覺得有點莫名其妙的功能
+// function heyFunc(myFunction){
+//     myFunction("Hi!!!")
+// }
+// heyFunc(alert)
+
+//匿名函數
+//有夠神奇的東西，本來把一個函數塞到另一個函數裏做參數已經夠神奇了
+//原來還可以把函數的名稱匿起來，繞一圈來執行函數 (???
+//我是真不懂
+// function heyFunc(myFunction){
+//     myFunction("Hi!!!")
+// }
+// heyFunc(function(message){
+//     console.log('message is:', message)
+// })
+
+//函數可以回傳函數，那麼就可以就做 立即函數 IIFE了
+//為什麼這樣用? 不想要讓以後寫的程式有機會污染到這些代碼用的變數
+// var sayHi = function(name){
+//     var strs = "Hi, " + name + " !"
+//     console.log(strs)
+// }("Peter");
+
+//hoisting 變數提升
+//不管在哪行用了變數，都視為在第一行宣告
+//var做到hoisting，但let不行
+// console.log(x);
+// var x = 10;
+// console.log(x);
+
+// console.log(y);
+// let y = 10;
+// console.log(y);
+
+// console.log(x) <- undefined
+// var x = 10; 
+// console.log(x); <- x=10
+// function sayHello(){
+//     console.log(x); <- undefined (function內自帶一層提升)
+//     var x = 20;
+//     console.log(x); <- x=20
+// }
+// sayHello()
+
+//not defined和undefined
+//not defined是調用一個完全沒出現過的變數的時候出現的
+//undefined是調用一個已經宣告了「它存在」的變數但沒給值的時候出現的
+//下面這種形式是可以的
+//因為變數的宣告「存不存在」有hoist到
+// x = 20;
+// var x;
+// console.log(x)
+
+//js object
+//this是指物件本身
+// var a = {
+//     key: ['Bob', 'Smith'],
+//     age: 32,
+//     gender: "male",
+//     interests: ['music','skiing'],
+//     greeting: function(){
+//         console.log('Hi! I\'m '+ this.name[0] + ".")
+//     }
+// }
+
+//js中的資料類別
+//基本類別:
+//- 數字
+//- 字串
+//- 布林
+//- 空值
+//- 未定義
+//- 苻號 (未教呢)
+
+//更多物件示範
+// var dog = {
+//     name: "Tracy",
+//     age: 20,
+//     bow: function(){
+//         console.log('bow bow')
+//     }
+// }
+// console.log(dog.name)
+// dog.bow()
+
+//這個同樣能印東西出來喔! (用下面這形式就和python的dictionary差不多)
+// console.log(dog['name'])
+
+// dog.color = 'white';
+// dog["size"] = "small";
+// console.log(dog)
+
+//陣列 array (同樣以0作第一個物品)
+//基本和python一樣 (但len在js是陣列的一種屬性)
+//pop就是移除一個元素
+//push是js版的append
+var arr = ['awa',154,'asta']
+for (i=0;i<=arr.length;i++){
+    console.log(arr[i])
+}
+arr.pop(154)
+console.log(arr)
+arr.push("cat")
+arr.push("cat")
+console.log(arr)
