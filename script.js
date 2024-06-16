189d441ce14f5b68f1c7e1294b1796155d7b314f
abung-ignis <86881903+abung-ignis@users.noreply.github.com>
2024-06-16 16:33:07 +0800

    More information updated

--- script.js
+++ script.js
@@ -1,31 +1,119 @@
-const table = document.querySelectorAll('td > div')
+const table = document.querySelectorAll('tr > td > div')
 const titles = document.getElementById('title')
 const the_table = document.getElementById('table')
+const display_block = document.getElementById('display_block')
+const little_title = document.getElementById('little_title')
+const little_subtitle = document.getElementById('little_subtitle')
+const god_info_first = document.getElementById('god_info_first')
+const god_info_second = document.getElementById('god_info_second')
+const god_info_third = document.getElementById('god_info_third')
+var bright = true
+var intervals = []
+var elemento = []
+var god_info = []
 
+fetch('./info.json')
+    .then((response) => response.json())
+    .then(function(json){
+        console.log(json);
+        god_info = json
+    });
+
+function element_info(the_id){
+    god_info_second.innerText = god_info[the_id]['duty']
+    var strs = god_info[the_id]['ps'].join("<br><br>")
+    god_info_third.innerHTML = `<p id="god_info_third">${strs}</p>`
+    var god_lst = []
+    document.getElementById('small_title_1').innerText = "神明名字"
+    document.getElementById('small_title_2').innerText = "神職"
+    document.getElementById('small_title_3').innerText = "小資訊"
+    document.getElementById('small_title_4').innerText = "氧化數"
+    for([index,names] of god_info[the_id]['god'].entries()){
+        if(god_info[the_id]['god_sex'][index] === "男"){
+            god_lst.push(`<span style="color: rgb(169, 169, 255)">${names}</span>`)
+        }else{
+            god_lst.push(`<span style="color: rgb(255, 169, 169)">${names}</span>`)
+        }
+    }
+    document.getElementById("another").innerHTML = `<tr id="another"></tr>`
+    for([index,oxidation] of god_info[the_id]['on'].entries()){
+        var td = document.createElement("td");
+        td.innerHTML = `<td id="not_wide"></td>`
+        document.getElementById("another").appendChild(td)
+        console.log(td)
+        var div = document.createElement("div")
+        div.innerHTML = `<div class="block" id="on" style="background-color: ${god_info[the_id]['color'][index]}">${oxidation}</div>`
+        div.style.backgroundColor = god_info[the_id]['color']
+        div.style.textDecoration = "underline";
+        console.log(oxidation in god_info[the_id]['usual_on'])
+        td.appendChild(div);
+        if(oxidation in god_info[the_id]['usual_on']){div.style.textDecoration = "underline";}
+        console.log(div.innerHTML)
+    }
+    god_info_first.innerHTML = god_lst.join("、")
+    little_title.innerText = the_id
+    little_subtitle.innerText = god_info[the_id]['name']
+} 
 
 titles.onclick = () => {
     table.forEach(chmelem => {
-        if(Number(chmelem.style.opacity) === 0.5){
-            chmelem.style.opacity = 1
-        }else{
+        if(bright&&chmelem.id!="on"){
             chmelem.style.opacity = 0.5
+        }else{
+            chmelem.style.opacity = 1
         }
     })
-    elem = table[(Math.floor(Math.random() * table.length))]
-    while(elem.class === 'text'){
-        elem = table[(Math.floor(Math.random() * table.length))]
+    bright?bright=false:bright=true;
+    if(bright===false){
+        for(var i=0;i<118;i++){
+            intervals.push(setTimeout(function(){
+                elem = table[(Math.floor(Math.random() * table.length))];
+                while(elem.getAttribute("class") === 'text'|| elem.id ==="on"){
+                    console.log("awa")
+                    elem = table[(Math.floor(Math.random() * table.length))];
+                };
+                display_block.style.backgroundColor = window.getComputedStyle(elem).getPropertyValue('background-color')
+                elem.style.opacity = 1;
+                elemento.push(elem.id);
+                little_title.innerText = elem.innerText
+                try{element_info(elem.id)}catch(awa){
+                }finally{}
+            },100+i*100))
+            intervals.push(setTimeout(function(){
+                console.log(elemento.lastIndexOf(elem.id));
+                if(elemento.lastIndexOf(elem.id)!==117){
+                    elem.style.opacity = 0.5
+                }else{
+                    elem.style.opacity = 1;
+                    console.log('awa')
+                };
+            },200+i*100))
+        }
+    }else{
+        for(const inter of intervals){
+            clearInterval(inter)
+            elemento = []
+        }
     }
-    elem.style.opacity = 1
 }
 
 table.forEach(chmelem => {
     chmelem.onclick = () => {
+        display_block.style.backgroundColor = window.getComputedStyle(chmelem).getPropertyValue('background-color')
+        console.log(window.getComputedStyle(chmelem).getPropertyValue('background-color'))
+        element_info(chmelem.id)
         if(chmelem.textContent === "Na"){
             chmelem.textContent = "鈉"
+            little_title.innerText = "Na"
+            little_subtitle.innerText = "鈉"
         }else if(chmelem.textContent === "鈉"){
             chmelem.textContent = "Na"
+            little_title.innerText = "Na"
+            little_subtitle.innerText = "鈉"
         }else if(chmelem.textContent === "H"){
             chmelem.textContent = "氫"
+            little_title.innerText = "H"
+            little_subtitle.innerText = "氫"
         }else if(chmelem.textContent === "氫"){
             chmelem.textContent = "H"
         }else if(chmelem.textContent === "He"){
