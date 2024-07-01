169c433d8e2e7fc2d3719565c94ec240b479a094
abung-ignis <86881903+abung-ignis@users.noreply.github.com>
2024-07-02 07:30:14 +0800

    js updated
    ✅
    - Now all data of an element will be shown on the website when its is clicked on the periodic table
    
    🟨
    - Some data is wrong and I am gonna to fix

---
+++ public/element.js
@@ -0,0 +1,310 @@
+// const cheerio = require("cheerio")
+// import * as elem_data from './info.json'
+
+//const elem_data = require('./info.json')
+
+export { name_convert }
+
+// async function obtainData(elem) {
+//   return fetch(
+//     `https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${elem}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => data.parse);
+// }
+
+// function delay(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// async function on(elem, important) {
+//   return new Promise((resolve, reject) => {
+//     obtainData(elem)
+//       .then((data) => {
+//         const $ = cheerio.load(data.text["*"]);
+//         const infobox = $(".infobox").first();
+//         const rows = infobox.find("tr");
+
+//         let oxidationState = "";
+
+//         rows.each((index, row) => {
+//           const th = $(row).find("th").first();
+//           if (th.text().trim() === "Oxidation states") {
+//             const td = $(row).find("td").first();
+//             oxidationState = td.text().trim();
+//           }
+//         });
+
+//         oxidationState = oxidationState
+//           .split(",")
+//           .map((element) =>
+//             element
+//           .replace(/\[.*?\]/g, "")
+//           .replace(/\(([^\)]+)\)/g, "$1")
+//           .replace(/\s/g, "")
+//           .replace(/[A-Za-z]/g, "").trim()
+//           );
+
+//         const boldedWords = [];
+
+//         $("b").each((index, element) => {
+//           const boldedText = $(element).text().trim();
+//           if (
+//             oxidationState.includes(boldedText) &&
+//             ["+1","+2","+3","+4","+5","+6","+7","+8","+9","0","-1", "-2","-3","-4","-5","-6","-7","-8","-9",].includes(boldedText)
+//           ) {
+//             boldedWords.push(boldedText);
+//           }
+//         });
+
+//         if (important) {
+//           console.log(boldedWords);
+//           resolve(boldedWords);
+//         } else {
+//           console.log(oxidationState);
+//           resolve(oxidationState);
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("An error occurred:", error);
+//         reject(error);
+//       });
+//   });
+// }
+
+// async function check_elem(elem,what) {
+//   return new Promise((resolve, reject) => {
+//     obtainData(elem)
+//       .then((data) => {
+//         const $ = cheerio.load(data.text["*"]);
+//         const infobox = $(".infobox").first();
+//         const rows = infobox.find("tr");
+
+//         let elem_value = "";
+
+//         rows.each((index, row) => {
+//           const rowText = $(row).text();
+//           if (rowText.includes(what)) {
+//             const td = $(row).find("td").first();
+//             elem_value = td.text().trim();
+//           }
+//         });
+
+//         elem_value = elem_value
+//                           .split(/[^0-9.-]/)
+//                           .filter((item) => item !== "");
+
+//         elem_value = Number(elem_value[0])
+
+//         resolve(elem_value);
+//       });
+//   });
+// }
+
+// async function check_multiple(elem, what) {
+//   return new Promise((resolve, reject) => {
+//     obtainData(elem)
+//       .then((data) => {
+//         const $ = cheerio.load(data.text["*"]);
+//         const infobox = $(".infobox").first();
+//         const rows = infobox.find("tr");
+
+//         let meltingPoint = "";
+
+//         rows.each((index, row) => {
+//           const rowText = $(row).text();
+//           if (rowText.includes(what)) {
+//             const td = $(row).find("td").first();
+//             meltingPoint = td.text().trim();
+//           }
+//         });
+
+//         console.log(what)
+//         console.log(meltingPoint)
+
+//         meltingPoint = meltingPoint
+//                           .split(/[^0-9.-]/)
+//                           .filter((item) => item !== "");
+
+//         for(const [i, value] of meltingPoint.entries()){
+//           meltingPoint[i] = Number(value)
+//         }
+
+//         resolve(meltingPoint);
+//       });
+//   });
+// }
+
+// function JsonGet(filename) {
+//   try {
+//     // Read existing JSON data from the file
+//     let jsonData = {};
+//     if (fs.existsSync(`${filename}.json`)) {
+//       const existingData = fs.readFileSync(`${filename}.json`, 'utf8');
+//       console.log(existingData)
+//       existingData!==""?jsonData = JSON.parse(existingData):jsonData = {}
+//     }
+
+//     return jsonData;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// }
+
+// async function JsonWrite(data, filename) {
+//   try {
+//     // Read existing JSON data from the file
+//     jsonData = JsonGet(filename)
+//     console.log(jsonData)
+
+//     // Merge existing data with the new data
+//     const mergedData = { ...jsonData, ...(await data) };
+
+//     // Convert the merged data to JSON format
+//     const jsondata = JSON.stringify(mergedData, null, 2);
+
+//     // Write the JSON data back to the file
+//     fs.writeFileSync(`${filename}.json`, jsondata);
+
+//     console.log(`Data added to ${filename}.json successfully.`);
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// }
+
+// async function infos_get(elem){
+//   return new Promise((resolve, reject) => {
+//       const elem_data = {};
+//       Promise.all([
+//         check_elem(elem, "Melting point").then((data) => {
+//           console.log("Melting point: " + data)
+//           elem_data["melting"] = data;
+//         }),
+//         check_elem(elem, "Boiling point").then((data) => {
+//           console.log("Boiling point :" + data)
+//           elem_data["boiling"] = data;
+//         }),
+//         check_elem(elem, "Density").then((data) => {
+//           console.log("Density: " + data)
+//           elem_data["density"] = data;
+//         }),
+//         check_elem(elem, "Heat of fusion").then((data) => {
+//           console.log("Heat of fusion: " + data)
+//           elem_data["lf"] = data;
+//         }),
+//         check_elem(elem, "Heat of vaporization").then((data) => {
+//           console.log("Heat of vaporization: " + data)
+//           elem_data["lv"] = data;
+//         }),
+//         check_elem(elem, "Atomic radius").then((data) => {
+//           console.log("Atomic radius: " + data)
+//           elem_data["atomic_radius"] = data;
+//         }),
+//         check_elem(elem, "Covalent radius").then((data) => {
+//           console.log("Covalent radius: " + data)
+//           elem_data["covalent_radius"] = data;
+//         }),
+//         check_elem(elem, "Van der Waals radius").then((data) => {
+//           console.log("Van der Waals radius: " + data)
+//           elem_data["van_der_waals_radius"] = data;
+//         }),
+//         check_elem(elem, "Electronegativity").then((data) => {
+//           console.log("Electronegativity: " + data)
+//           elem_data["electronegativity"] = data;
+//         }),
+//         on(elem, false).then((data) => {
+//           console.log("On: " + data)
+//           elem_data["on"] = data;
+//         }),
+//         on(elem, true).then((data) => {
+//           console.log("Important ons: " + data)
+//           elem_data["important_on"] = data;
+//         }),
+//         check_multiple(elem,"Electrons per shell").then((data) => {
+//           console.log("Electrons per shell: " + data)
+//           elem_data["electron_shell"] = data;
+//         }),
+//         check_multiple(elem,"Ionization energies").then((data) => {
+//           console.log("Ionization energies: " + data)
+//           elem_data["ionization_energies"] = data;
+//         })
+//     ])
+//       .then(() => {
+//         dict = {}
+
+//         dict[elem] = elem_data
+//         resolve(dict);
+//       })
+//       .catch((error) => {
+//         reject(error);
+//       });
+//   });
+// }
+
+async function name_convert(name) {
+  let response = await fetch('./info.json');
+  let elem_data = await response.json();
+
+  for (let element in elem_data) {
+    if (elem_data[element]["name"] === name) {
+      console.log("awa");
+      return element;
+    } else if (element === name) {
+      return elem_data[element]["name"];
+    }
+  }
+}
+
+//name_convert("K").then((data) => { console.log(data) });
+
+// check_elem("Hydrogen","Critical point").then((data) => {
+//   console.log(data)
+// })
+
+// check_multiple("Sodium","Electrons per shell").then((data) => {
+//   console.log(data)
+// })
+
+// on("nitrogen", true).then(
+//   (data) => console.log(data)
+// )
+
+// (async function element_state(){
+//   for(elem of ["Hydrogen","Helium","Lithium","Beryllium","Boron",
+//     "Carbon","Nitrogen","Oxygen","Fluorine","Neon","Sodium","Magnesium",
+//     "Aluminium","Silicon","Phosphorus","Sulfur","Chlorine","Argon","Potassium",
+//     "Calcium","Scandium","Titanium","Vanadium","Chromium","Manganese","Iron",
+//     "Cobalt","Nickel","Copper","Zinc","Gallium","Germanium","Arsenic","Selenium",
+//     "Bromine","Krypton","Rubidium","Strontium","Yttrium","Zirconium","Niobium",
+//     "Molybdenum","Technetium","Ruthenium","Rhodium","Palladium","Silver","Cadmium",
+//     "Indium","Tin","Antimony","Tellurium","Iodine","Xenon","Caesium","Barium",
+//     "Lanthanum","Cerium","Praseodymium","Neodymium","Promethium","Samarium","Europium",
+//     "Gadolinium","Terbium","Dysprosium","Holmium","Erbium","Thulium","Ytterbium","Lutetium",
+//     "Hafnium","Tantalum","Tungsten","Rhenium","Osmium","Iridium","Platinum","Gold",
+//     "Mercury (element)","Thallium","Lead","Bismuth","Polonium","Astatine","Radon","Francium",
+//     "Radium","Actinium","Thorium","Protactinium","Uranium","Neptunium","Plutonium","Americium",
+//     "Curium","Berkelium","Californium","Einsteinium","Fermium","Mendelevium","Nobelium",
+//     "Lawrencium","Rutherfordium","Dubnium","Seaborgium","Bohrium","Hassium","Meitnerium",
+//     "Darmstadtium","Roentgenium","Copernicium","Nihonium","Flerovium","Moscovium","Livermorium",
+//     "Tennessine","Oganesson"
+//   ]){
+//     try {
+//       const data = await infos_get(elem);
+//       JsonWrite(data, "extra_info");
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// })()
+
+// fetch(
+//   `https://pubchem.ncbi.nlm.nih.gov/rest/pug/periodictable/JSON`
+// )
+//   .then(function(response){return response.text()})
+//   .then(function(data){
+//     console.log(data)
+//     mw = JSON.parse(data)
+//     console.log(mw)
+//   });
--- public/script.js
+++ public/script.js
@@ -16,17 +16,28 @@ var extra_info = []
 
 fetch('./info.json')
     .then((response) => response.json())
-    .then(function(json){
+    .then(function (json) {
         console.log(json);
         god_info = json
     });
 
-function element_info(the_id){
-    god_info_second.innerText = god_info[the_id]['duty']
+function fill_data(the_id,what,where,unit){
+    if (god_info[the_id][what] !== null) {
+        document.getElementById(where).innerHTML = `<p class= "infos" id="${where}">${god_info[the_id][what]}${unit}</p>`
+    } else {
+        document.getElementById(where).innerHTML = `<p class= "infos" id="${where}">未知</p>`
+    }
+}
+
+function element_info(the_id) {
+    if(god_info[the_id]['duty']===null){
+        god_info_second.innerText = "無"
+    }else{
+        god_info_second.innerText = god_info[the_id]['duty']
+    }
     // var strs = god_info[the_id]['ps'].join("<br><br>")
     // god_info_third.innerHTML = `<p id="god_info_third">${strs}</p>`
     var god_lst = []
-    console.log(god_info[the_id]['melting']-273.15)
     document.getElementById('small_title_1').innerText = "神明名字"
     document.getElementById('small_title_2').innerText = "神職"
     // document.getElementById('small_title_3').innerText = "小資訊"
@@ -34,70 +45,93 @@ function element_info(the_id){
     document.getElementById('small_title_3').innerText = "熔點"
     document.getElementById('small_title_5').innerText = "沸點"
     document.getElementById('small_title_6').innerText = "電子排序"
-    document.getElementById('element_info_1').innerHTML = `<p class= "infos" id="element_info_1">${(god_info[the_id]['melting']-273.15).toFixed(2)}℃<br>(${(god_info[the_id]['melting']).toFixed(2)}K)</p>`
-    document.getElementById('element_info_2').innerHTML = `<p class= "infos" id="element_info_2">${(god_info[the_id]['boiling']-273.15).toFixed(2)}℃<br>(${(god_info[the_id]['boiling']).toFixed(2)}K)</p>`
-    console.log(god_info[the_id]['god'])
-    for(let names of god_info[the_id]['god']){
-        if(god_info[the_id]['god_sex'][god_info[the_id]['god_sex'].indexOf(names)] === "男"){
+    document.getElementById('small_title_7').innerText = "火焰顏色"
+    document.getElementById('small_title_8').innerText = "密度"
+    document.getElementById('small_title_9').innerText = "電負度"
+    document.getElementById('small_title_10').innerText = "共價半徑"
+    document.getElementById('small_title_11').innerText = "原子半徑"
+    document.getElementById('small_title_12').innerText = "范德華半徑"
+    if (god_info[the_id]['melting'] !== null) {
+        document.getElementById('element_info_1').innerHTML = `<p class= "infos" id="element_info_1">${(god_info[the_id]['melting'] - 273.15).toFixed(2)}℃<br>(${(god_info[the_id]['melting']).toFixed(2)}K)</p>`
+    } else {
+        document.getElementById('element_info_1').innerHTML = `<p class= "infos" id="element_info_1">未知</p>`
+    }
+    if (god_info[the_id]['boiling'] !== null) {
+        document.getElementById('element_info_2').innerHTML = `<p class= "infos" id="element_info_2">${(god_info[the_id]['boiling'] - 273.15).toFixed(2)}℃<br>(${(god_info[the_id]['boiling']).toFixed(2)}K)</p>`
+    } else {
+        document.getElementById('element_info_2').innerHTML = `<p class= "infos" id="element_info_2">未知</p>`
+    }
+    fill_data(the_id,"density","element_info_3","g/cm³")
+    fill_data(the_id,"electronegativity","god_info_fourth","")
+    fill_data(the_id,"covalent_radius","god_info_fifth","pm")
+    fill_data(the_id,"atomic_radius","god_info_sixth","pm")
+    fill_data(the_id,"van_der_waals_radius","god_info_seventh","pm")
+    //console.log(god_info[the_id]['god'])
+    for (let names of god_info[the_id]['god']) {
+        if (god_info[the_id]['god_sex'][god_info[the_id]['god'].indexOf(names)] === "男") {
             god_lst.push(`<span style="color: rgb(169, 169, 255)">${names}</span>`)
-        }else{
+        } else {
             god_lst.push(`<span style="color: rgb(255, 169, 169)">${names}</span>`)
         }
     }
     document.getElementById("another").innerHTML = `<tr id="another"></tr>`
     document.getElementById("element_another").innerHTML = `<tr id="element_another"></tr>`
-    for(let oxidation of god_info[the_id]['on']){
+    for (let oxidation of god_info[the_id]['on']) {
         var td = document.createElement("td");
         td.innerHTML = `<td id="not_wide"></td>`
         document.getElementById("another").appendChild(td)
-        console.log(td)
-        if(oxidation != "0"){
-            oxidation = oxidation[1]+oxidation[0]
+        //console.log(td)
+        var ion = "0"
+        if (oxidation != "0") {
+            ion = oxidation[1] + oxidation[0]
         }
-        let colour = god_info[the_id]['color'][oxidation]
-        console.log(oxidation)
-        console.log(colour)
-        if(colour === undefined){
+        let colour = god_info[the_id]['color'][ion]
+        //console.log(colour)
+        if (colour === undefined) {
             colour = ["無色"]
         }
         var div = document.createElement("div")
-        if(god_info[the_id]['important_on'].indexOf(oxidation) != -1){
+        if (god_info[the_id]['important_on'].indexOf(oxidation) != -1) {
             div.innerHTML = `<div class="block" id="on" style="background-color: ${colour[1]}; color: yellow;">${oxidation}</div>`
-        }else{
+        } else {
             div.innerHTML = `<div class="block" id="on" style="background-color: ${colour[1]};">${oxidation}</div>`
         }
         td.appendChild(div);
-        console.log(div.innerHTML)
+        //console.log(div.innerHTML)
     }
-    for([index,shells] of god_info[the_id]['shells'].entries()){
+    console.log(god_info[the_id]['flame_color'][1])
+    document.getElementById("god_info_third").innerHTML = `<p class= "infos" id="god_info_third" style="color: ${god_info[the_id]['flame_color'][1]};">${god_info[the_id]['flame_color'][0]}</p>`
+    //console.log(god_info[the_id]['electron_shell'])
+    for (let shells of god_info[the_id]['electron_shell']) {
         var td = document.createElement("td");
         td.innerHTML = `<td id="another_not_wide"></td>`
         document.getElementById("element_another").appendChild(td)
-        console.log(td)
+        //console.log(td)
         var div = document.createElement("div")
         div.innerHTML = `<div class="block" id="electron">${shells}</div>`
         td.appendChild(div);
-        console.log(div.innerHTML)
+        //console.log(div.innerHTML)
     }
     god_info_first.innerHTML = god_lst.join("、")
     little_title.innerText = the_id
     little_subtitle.innerText = god_info[the_id]['name']
-} 
+}
 
 titles.onclick = () => {
     table.forEach(chmelem => {
-        if(bright&&chmelem.id!="on"){
+        if (bright && chmelem.id != "on") {
             chmelem.style.opacity = 0.5
-        }else{
+        } else {
             chmelem.style.opacity = 1
         }
     })
-    bright?bright=false:bright=true;
-    if(bright===false){
-        for(var i=0;i<118;i++){
-            intervals.push(setTimeout(function(){
+    bright ? bright = false : bright = true;
+    if (bright === false) {
+        for (var i = 0; i < 118; i++) {
+            var elem = "test"
+            intervals.push(setTimeout(function () {
                 elem = table[(Math.floor(Math.random() * table.length))];
-                while(elem.getAttribute("class") === 'text'|| elem.id ==="on"){
+                while (elem.getAttribute("class") === 'text' || elem.id === "on") {
                     console.log("awa")
                     elem = table[(Math.floor(Math.random() * table.length))];
                 };
@@ -105,21 +139,21 @@ titles.onclick = () => {
                 elem.style.opacity = 1;
                 elemento.push(elem.id);
                 little_title.innerText = elem.innerText
-                try{element_info(elem.id)}catch(awa){
-                }finally{}
-            },100+i*100))
-            intervals.push(setTimeout(function(){
+                try { element_info(elem.id) } catch (awa) {
+                } finally { }
+            }, 100 + i * 100))
+            intervals.push(setTimeout(function () {
                 console.log(elemento.lastIndexOf(elem.id));
-                if(elemento.lastIndexOf(elem.id)!==117){
+                if (elemento.lastIndexOf(elem.id) !== 117) {
                     elem.style.opacity = 0.5
-                }else{
+                } else {
                     elem.style.opacity = 1;
                     console.log('awa')
                 };
-            },200+i*100))
+            }, 200 + i * 100))
         }
-    }else{
-        for(const inter of intervals){
+    } else {
+        for (const inter of intervals) {
             clearInterval(inter)
             elemento = []
         }
@@ -128,9 +162,13 @@ titles.onclick = () => {
 
 table.forEach(chmelem => {
     chmelem.onclick = () => {
+        if(chmelem.className === "text"){return}
         display_block.style.backgroundColor = window.getComputedStyle(chmelem).getPropertyValue('background-color')
         console.log(window.getComputedStyle(chmelem).getPropertyValue('background-color'))
+        console.log(chmelem.textContent)
         element_info(chmelem.id)
-        chmelem.textContent = name.name_convert(chmelem.textContent)
+        name.name_convert(chmelem.innerText).then((result) => {
+            chmelem.textContent = result
+        })
     }
 })
